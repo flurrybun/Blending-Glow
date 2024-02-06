c7c8ab89711b3c41ae64db7971c35678c06f2d7a
flurrybun <parkercasey007@gmail.com>
2024-02-06 00:30:54 -0500

    v1.0.1: use node ids instead of defining them myself

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,6 @@ class $modify(PlayerObject) {
     bool init(int p0, int p1, GJBaseGameLayer* p2, cocos2d::CCLayer* p3, bool p4) {
         if (!PlayerObject::init(p0, p1, p2, p3, p4)) return false;
 
-        auto node = as<CCNode*>(this->getChildren()->objectAtIndex(0));
-        as<CCSprite*>(as<CCSprite*>(node->getChildren()->objectAtIndex(6))->getChildren()->objectAtIndex(1))->setID("gamemode-glow");
-        as<CCSprite*>(as<CCSprite*>(node->getChildren()->objectAtIndex(6))->getChildren()->objectAtIndex(2))->setID("ship-glow");
-
         return true;
     }
 
