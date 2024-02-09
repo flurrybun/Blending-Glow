683e4cd489df3045393f698b6ab45fe87d4cb002
flurrybun <parkercasey007@gmail.com>
2024-02-08 19:03:14 -0500

    replace node ids with member variables

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,15 @@
 using namespace geode::prelude;
 
 class $modify(PlayerObject) {
-    bool init(int p0, int p1, GJBaseGameLayer* p2, cocos2d::CCLayer* p3, bool p4) {
-        if (!PlayerObject::init(p0, p1, p2, p3, p4)) return false;
-
-        return true;
-    }
-
     void updatePlayerArt() {
         PlayerObject::updatePlayerArt();
 
         ccBlendFunc blendFunc = {GL_ONE, GL_ONE_MINUS_CONSTANT_ALPHA};
 
-        auto gamemodeGlow = as<CCSprite*>(PlayerObject::getChildByIDRecursive("gamemode-glow"));
-        auto shipGlow = as<CCSprite*>(PlayerObject::getChildByIDRecursive("ship-glow"));
+        auto iconGlow = m_iconGlow;
+        auto vehicleGlow = m_vehicleGlow;
 
-        if (gamemodeGlow) gamemodeGlow->setBlendFunc(blendFunc);
-        if (shipGlow) shipGlow->setBlendFunc(blendFunc);
+        if (iconGlow) iconGlow->setBlendFunc(blendFunc);
+        if (vehicleGlow) vehicleGlow->setBlendFunc(blendFunc);
     }
 };
