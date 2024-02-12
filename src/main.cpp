caad8712d8fd71892e7003c476449397be483bb7
flurrybun <parkercasey007@gmail.com>
2024-02-12 15:10:54 -0500

    add option to increase glow brightness
    resolves #1

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,21 @@
 using namespace geode::prelude;
 
 class $modify(PlayerObject) {
+    bool isBrighterGlow = Mod::get()->getSettingValue<bool>("brighter-glow");
+    
     void updatePlayerArt() {
         PlayerObject::updatePlayerArt();
+
+        if (m_hasGlow == false) return;
+
+        if (isBrighterGlow) {
+            int r = std::min(static_cast<int>(m_glowColor.r * 1.5), 255);
+            int g = std::min(static_cast<int>(m_glowColor.g * 1.5), 255);
+            int b = std::min(static_cast<int>(m_glowColor.b * 1.5), 255);
+            
+            m_glowColor = ccc3(r, g, b);
+            updateGlowColor();
+        }
 
         ccBlendFunc blendFunc = {GL_ONE, GL_ONE_MINUS_CONSTANT_ALPHA};
 
